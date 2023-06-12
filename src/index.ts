806e03a8d552ad4fdbfa325eed0ab6ee10620d21
Hyeonjong <nianelo4@gmail.com>
2023-06-12 22:03:51 +0900

    feat: define plugin languages

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import type { Plugin } from 'prettier';
 
+import { languages } from './languages';
 import { options } from './options';
 
 const braceStylePlugin: Plugin = {
+  languages,
   options,
 };
 
---
+++ src/languages.ts
@@ -0,0 +1,8 @@
+import type { SupportLanguage } from 'prettier';
+
+export const languages: SupportLanguage[] = [
+  {
+    name: 'typescript',
+    parsers: ['typescript'],
+  },
+];
