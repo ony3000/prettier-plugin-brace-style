09aee809f2893b61b656dc6cd8cac184f27dc9b0
Hyeonjong <nianelo4@gmail.com>
2023-06-13 00:03:42 +0900

    test: add allman test

---
+++ tests/allman.test.ts
@@ -0,0 +1,55 @@
+import prettier from 'prettier';
+
+import { braceStylePlugin } from '@/index';
+import { exampleCode } from './fixtures';
+
+const expectedResult = `function foo()
+{
+  return true;
+}
+
+if (foo)
+{
+  bar();
+}
+
+try
+{
+  somethingRisky();
+}
+catch (e)
+{
+  handleError();
+}
+
+if (foo)
+{
+  bar();
+}
+else
+{
+  baz();
+}
+
+class C
+{
+  static
+  {
+    foo();
+  }
+}
+
+if (foo) bar();
+else if (baz) boom();
+`;
+
+test('Allman style test', () => {
+  const formattedCode = prettier.format(exampleCode, {
+    parser: 'typescript',
+    plugins: [braceStylePlugin],
+    // @ts-ignore
+    braceStyle: 'allman',
+  });
+
+  expect(formattedCode).toBe(expectedResult);
+});
