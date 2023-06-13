f59c4a0ba0143096cdeb40c9c4b069116b69cb5b
Hyeonjong <nianelo4@gmail.com>
2023-06-14 00:05:21 +0900

    test: add 'if' statement tests

---
+++ tests/if/1tbs.test.ts
@@ -0,0 +1,56 @@
+import { format, baseOptions } from '../settings';
+import { ifCode, ifElseCode, ifElseifElseCode, ifElseCodeWithComment } from './fixtures';
+
+const options = {
+  ...baseOptions,
+  braceStyle: '1tbs',
+};
+
+describe('1tbs - if statements', () => {
+  test('if', () => {
+    const expectedResult = `if (foo) {
+  bar();
+}
+`;
+
+    expect(format(ifCode, options)).toBe(expectedResult);
+  });
+
+  test('if...else', () => {
+    const expectedResult = `if (foo) {
+  bar();
+} else {
+  baz();
+}
+`;
+
+    expect(format(ifElseCode, options)).toBe(expectedResult);
+  });
+
+  test('if...elseif...else', () => {
+    const expectedResult = `if (foo) {
+  bar();
+} else if (baz) {
+  qux();
+} else {
+  quux();
+}
+`;
+
+    expect(format(ifElseifElseCode, options)).toBe(expectedResult);
+  });
+
+  test('if...else (with comment)', () => {
+    const expectedResult = `// foo is truthy
+if (foo) {
+  bar();
+}
+// foo is falsy
+else {
+  baz();
+}
+`;
+
+    expect(format(ifElseCodeWithComment, options)).toBe(expectedResult);
+  });
+});
---
+++ tests/if/allman.test.ts
@@ -0,0 +1,67 @@
+import { format, baseOptions } from '../settings';
+import { ifCode, ifElseCode, ifElseifElseCode, ifElseCodeWithComment } from './fixtures';
+
+const options = {
+  ...baseOptions,
+  braceStyle: 'allman',
+};
+
+describe('allman - if statements', () => {
+  test('if', () => {
+    const expectedResult = `if (foo)
+{
+  bar();
+}
+`;
+
+    expect(format(ifCode, options)).toBe(expectedResult);
+  });
+
+  test('if...else', () => {
+    const expectedResult = `if (foo)
+{
+  bar();
+}
+else
+{
+  baz();
+}
+`;
+
+    expect(format(ifElseCode, options)).toBe(expectedResult);
+  });
+
+  test('if...elseif...else', () => {
+    const expectedResult = `if (foo)
+{
+  bar();
+}
+else if (baz)
+{
+  qux();
+}
+else
+{
+  quux();
+}
+`;
+
+    expect(format(ifElseifElseCode, options)).toBe(expectedResult);
+  });
+
+  test('if...else (with comment)', () => {
+    const expectedResult = `// foo is truthy
+if (foo)
+{
+  bar();
+}
+// foo is falsy
+else
+{
+  baz();
+}
+`;
+
+    expect(format(ifElseCodeWithComment, options)).toBe(expectedResult);
+  });
+});
