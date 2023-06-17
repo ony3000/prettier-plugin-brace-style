47bd104d5a82146a62c99f20921a9370857d23d5
Hyeonjong <nianelo4@gmail.com>
2023-06-17 15:01:42 +0900

    test: add 'while' statement tests

---
+++ tests/while/1tbs.test.ts
@@ -0,0 +1,33 @@
+import { format, baseOptions } from '../settings';
+import { whileCode, doWhileCode } from './fixtures';
+
+const options = {
+  ...baseOptions,
+  braceStyle: '1tbs',
+};
+
+describe('1tbs - while statements', () => {
+  test('while', () => {
+    const expectedResult = `let n = 0;
+
+while (n < 3) {
+  n++;
+}
+`;
+
+    expect(format(whileCode, options)).toBe(expectedResult);
+  });
+
+  test('do...while', () => {
+    const expectedResult = `let result = "";
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+`;
+
+    expect(format(doWhileCode, options)).toBe(expectedResult);
+  });
+});
---
+++ tests/while/fixtures.ts
@@ -0,0 +1,17 @@
+export const whileCode = `
+let n = 0;
+
+while (n < 3) {
+  n++;
+}
+`;
+
+export const doWhileCode = `
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+`;
