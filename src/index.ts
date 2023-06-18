4e14c5a4283a101fe6f339ab1e68e006a1de1eff
Hyeonjong <nianelo4@gmail.com>
2023-06-18 20:26:09 +0900

    chore: remove unnecessary export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import type { Plugin } from 'prettier';
 
-import { languages } from './languages';
 import { parsers } from './parsers';
 import { printers } from './printers';
 import { options } from './options';
 
 const braceStylePlugin: Plugin = {
-  languages,
   parsers,
   printers,
   options,
--- src/languages.ts
+++
@@ -1,8 +0,0 @@
-import type { SupportLanguage } from 'prettier';
-
-export const languages: SupportLanguage[] = [
-  {
-    name: 'typescript',
-    parsers: ['typescript'],
-  },
-];
