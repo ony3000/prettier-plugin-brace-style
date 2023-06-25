e1088a4a94d189eba949869aa4e57b03618d2ef2
Hyeonjong <nianelo4@gmail.com>
2023-06-25 17:32:01 +0900

    revert: "test: add tailwindcss plugin tests"
    This reverts commit 374d857081197b54ebd9a3b65a98f2495ecafe47.

--- tests/settings.ts
+++ tests/settings.ts
@@ -1,10 +1,4 @@
 import type { Options } from 'prettier';
-import {
-  parsers as tailwindcssParsers,
-  printers as tailwindcssPrinters,
-  options as tailwindcssOptions,
-  // @ts-ignore
-} from 'prettier-plugin-tailwindcss';
 
 import braceStylePlugin from '@/index';
 
@@ -12,10 +6,4 @@ export { format } from 'prettier';
 
 export const baseOptions: Options = {
   plugins: [braceStylePlugin],
-};
-
-export const tailwindcssPlugin = {
-  parsers: tailwindcssParsers,
-  printers: tailwindcssPrinters,
-  options: tailwindcssOptions,
 };
