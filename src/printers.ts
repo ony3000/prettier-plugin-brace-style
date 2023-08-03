0d22ef3df2d5c268470248da275e8ee3cd1d11fd
Hyeonjong <nianelo4@gmail.com>
2023-08-03 21:40:51 +0900

    feat: implements formatting for '1tbs' option

--- src/printers.ts
+++ src/printers.ts
@@ -1,248 +1,45 @@
-import type { AstPath, ParserOptions, Doc, Printer } from 'prettier';
-import { doc } from 'prettier';
+import type { AstPath, ParserOptions, Doc, Printer, Plugin } from 'prettier';
+import { format } from 'prettier';
 
-import { extractPrinter, makeCommentContext } from './utils';
+function createPrinter(parserName: 'babel' | 'typescript'): Printer {
+  function main(
+    path: AstPath,
+    options: ParserOptions,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    print: (path: AstPath) => Doc,
+  ): Doc {
+    const plugins = options.plugins.filter((plugin) => typeof plugin !== 'string') as Plugin[];
+    const pluginCandidate = plugins.find((plugin) => plugin.parsers?.[parserName]);
 
-const { hardline } = doc.builders;
-
-const printerErrorMessage =
-  'There is no default printer or the function to look for does not exist.';
-
-let defaultPrinter: Printer | undefined;
-
-function printWithBraceStyle(
-  path: AstPath,
-  options: ParserOptions,
-  print: (path: AstPath) => Doc,
-): Doc {
-  const node = path.getValue();
-
-  if (!defaultPrinter && node) {
-    if (
-      (options.parser === 'babel' && node.type === 'File') ||
-      (options.parser === 'typescript' && node.type === 'Program')
-    ) {
-      defaultPrinter = extractPrinter(options);
+    if (!pluginCandidate) {
+      throw new Error('A plugin with the given parser does not exist.');
     }
-  }
 
-  if (!defaultPrinter) {
-    throw new Error('Default printer does not exist.');
-  }
+    const node = path.getValue();
 
-  const defaultDoc = defaultPrinter.print(path, options, print);
-
-  if (node) {
-    switch (node.type) {
-      case 'BlockStatement': {
-        const parentNode = path.getParentNode();
-        const refinedDoc = (defaultDoc as Doc[]).slice();
-
-        // @ts-ignore
-        if (options.braceStyle === 'allman' && parentNode.type !== 'SwitchCase') {
-          refinedDoc.unshift(hardline);
-        }
-
-        return refinedDoc;
-      }
-      case 'IfStatement': {
-        const refinedDoc = (defaultDoc as Doc[]).slice();
-
-        // @ts-ignore
-        if (options.braceStyle === 'stroustrup' || options.braceStyle === 'allman') {
-          refinedDoc.forEach((docItem, index, array) => {
-            if (docItem === 'else') {
-              // eslint-disable-next-line no-param-reassign
-              array[index - 1] = hardline;
-            }
-          });
-        }
-
-        return refinedDoc;
-      }
-      case 'SwitchStatement':
-      case 'StaticBlock': {
-        const refinedDoc = (defaultDoc as Doc[]).slice();
-
-        // @ts-ignore
-        if (options.braceStyle === 'allman') {
-          refinedDoc.splice(1, 1, hardline, '{');
-        }
-
-        return refinedDoc;
-      }
-      case 'TryStatement': {
-        const refinedDoc = (defaultDoc as Doc[]).slice();
-
-        // @ts-ignore
-        if (options.braceStyle === 'stroustrup' || options.braceStyle === 'allman') {
-          refinedDoc.forEach((docItem) => {
-            if (Array.isArray(docItem)) {
-              const [firstDoc, secondDoc] = docItem;
-
-              if (typeof firstDoc === 'string' && Array.isArray(secondDoc)) {
-                const matchResult = firstDoc.match(/(finally.*)/);
-
-                if (matchResult) {
-                  docItem.splice(0, 1, hardline, matchResult[1]);
-                } else {
-                  // catch clause
-                  docItem.splice(0, 1, hardline);
-                }
-              }
-            }
-          });
-        }
-
-        return refinedDoc;
-      }
-      case 'ClassBody': {
-        const refinedDoc = (defaultDoc as Doc[]).slice();
-
-        // @ts-ignore
-        if (options.braceStyle === 'allman') {
-          refinedDoc.unshift(hardline);
-        }
-
-        return refinedDoc;
-      }
-      default: {
-        return defaultDoc;
-      }
+    if (node?.comments) {
+      node.comments.forEach((comment: any) => {
+        // eslint-disable-next-line no-param-reassign
+        comment.printed = true;
+      });
     }
+
+    const { originalText } = options;
+    const formattedText = format(originalText, {
+      ...options,
+      plugins: [pluginCandidate],
+      rangeEnd: Infinity,
+    });
+
+    return formattedText;
   }
 
-  return defaultDoc;
-}
-
-function canAttachComment(node: any): boolean {
-  return node.type && node.type !== 'comment';
-}
-
-function isBlockCommentWrapper(node: any): boolean {
-  if (defaultPrinter?.isBlockComment) {
-    return defaultPrinter.isBlockComment(node);
-  }
-
-  throw new Error(`[isBlockCommentWrapper] ${printerErrorMessage}`);
-}
-
-function printCommentWrapper(commentPath: AstPath, options: ParserOptions): Doc {
-  if (defaultPrinter?.printComment) {
-    return defaultPrinter.printComment(commentPath, options);
-  }
-
-  throw new Error(`[printCommentWrapper] ${printerErrorMessage}`);
-}
-
-function ownLineWrapper(
-  commentNode: any,
-  text: string,
-  options: ParserOptions,
-  ast: any,
-  isLastComment: boolean,
-): boolean {
-  if (!defaultPrinter && ast) {
-    if (
-      (options.parser === 'babel' && ast.type === 'File') ||
-      (options.parser === 'typescript' && ast.type === 'Program')
-    ) {
-      defaultPrinter = extractPrinter(options);
-    }
-  }
-
-  if (defaultPrinter?.handleComments?.ownLine) {
-    const commentContext = makeCommentContext(commentNode, text, options, ast, isLastComment);
-
-    return defaultPrinter.handleComments.ownLine(commentContext, text, options, ast, isLastComment);
-  }
-
-  throw new Error(`[ownLineWrapper] ${printerErrorMessage}`);
-}
-
-function endOfLineWrapper(
-  commentNode: any,
-  text: string,
-  options: ParserOptions,
-  ast: any,
-  isLastComment: boolean,
-): boolean {
-  if (!defaultPrinter && ast) {
-    if (
-      (options.parser === 'babel' && ast.type === 'File') ||
-      (options.parser === 'typescript' && ast.type === 'Program')
-    ) {
-      defaultPrinter = extractPrinter(options);
-    }
-  }
-
-  if (defaultPrinter?.handleComments?.endOfLine) {
-    const commentContext = makeCommentContext(commentNode, text, options, ast, isLastComment);
-
-    return defaultPrinter.handleComments.endOfLine(
-      commentContext,
-      text,
-      options,
-      ast,
-      isLastComment,
-    );
-  }
-
-  throw new Error(`[endOfLineWrapper] ${printerErrorMessage}`);
-}
-
-function remainingWrapper(
-  commentNode: any,
-  text: string,
-  options: ParserOptions,
-  ast: any,
-  isLastComment: boolean,
-): boolean {
-  if (!defaultPrinter && ast) {
-    if (
-      (options.parser === 'babel' && ast.type === 'File') ||
-      (options.parser === 'typescript' && ast.type === 'Program')
-    ) {
-      defaultPrinter = extractPrinter(options);
-    }
-  }
-
-  if (defaultPrinter?.handleComments?.remaining) {
-    const commentContext = makeCommentContext(commentNode, text, options, ast, isLastComment);
-
-    return defaultPrinter.handleComments.remaining(
-      commentContext,
-      text,
-      options,
-      ast,
-      isLastComment,
-    );
-  }
-
-  throw new Error(`[remainingWrapper] ${printerErrorMessage}`);
+  return {
+    print: main,
+  };
 }
 
 export const printers: { [astFormat: string]: Printer } = {
-  'babel-ast': {
-    print: printWithBraceStyle,
-    canAttachComment,
-    isBlockComment: isBlockCommentWrapper,
-    printComment: printCommentWrapper,
-    handleComments: {
-      ownLine: ownLineWrapper,
-      endOfLine: endOfLineWrapper,
-      remaining: remainingWrapper,
-    },
-  },
-  'typescript-ast': {
-    print: printWithBraceStyle,
-    canAttachComment,
-    isBlockComment: isBlockCommentWrapper,
-    printComment: printCommentWrapper,
-    handleComments: {
-      ownLine: ownLineWrapper,
-      endOfLine: endOfLineWrapper,
-      remaining: remainingWrapper,
-    },
-  },
+  'babel-ast': createPrinter('babel'),
+  'typescript-ast': createPrinter('typescript'),
 };
