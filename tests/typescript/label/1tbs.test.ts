616f94a21e3916e2076f820c9003b3df8be5cf6a
Hyeonjong <nianelo4@gmail.com>
2023-08-06 18:37:49 +0900

    test: add various tests for typescript parser

---
+++ tests/typescript/label/1tbs.test.ts
@@ -0,0 +1,35 @@
+import type { Fixture } from '../../settings';
+import { format, baseOptions } from '../../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'typescript',
+  braceStyle: '1tbs',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'labeled block',
+    input: `foo: {
+  console.log("face");
+  break foo;
+  console.log("this will not be executed");
+}
+console.log("swap");`,
+    output: `foo: {
+  console.log("face");
+  break foo;
+  console.log("this will not be executed");
+}
+console.log("swap");
+`,
+  },
+];
+
+describe('typescript/label/1tbs', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
