278a0ec52f514ff5dc1c3d7f1e9082c570709521
Hyeonjong <nianelo4@gmail.com>
2023-08-06 18:30:41 +0900

    test: add various tests for other options

---
+++ tests/babel/function/stroustrup.test.ts
@@ -0,0 +1,258 @@
+import type { Fixture } from '../../settings';
+import { format, baseOptions } from '../../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'babel',
+  braceStyle: 'stroustrup',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'empty function',
+    input: `function foo() {}`,
+    output: `function foo() {}
+`,
+  },
+  {
+    name: 'empty function with comment #1',
+    input: `function /*comment1*/ foo() {}`,
+    output: `function /*comment1*/ foo() {}
+`,
+  },
+  {
+    name: 'empty function with comment #2',
+    input: `function
+/*comment2*/
+foo() {}`,
+    output: `function /*comment2*/
+foo() {}
+`,
+  },
+  {
+    name: 'empty function with comment #3',
+    input: `function foo(/*comment3*/) {}`,
+    output: `function foo(/*comment3*/) {}
+`,
+  },
+  {
+    name: 'empty function with comment #4',
+    input: `function foo() /*comment4*/ {}`,
+    output: `function foo() /*comment4*/ {}
+`,
+  },
+  {
+    name: 'empty function with comment #5',
+    input: `function foo() {} /*comment5*/`,
+    output: `function foo() {} /*comment5*/
+`,
+  },
+  {
+    name: 'empty function expression',
+    input: `const foo = function () {}`,
+    output: `const foo = function () {};
+`,
+  },
+  {
+    name: 'empty function expression with comment #1',
+    input: `const foo = function /*comment1*/ () {}`,
+    output: `const foo = function () /*comment1*/ {};
+`,
+  },
+  {
+    name: 'empty function expression with comment #2',
+    input: `const foo = function (/*comment2*/) {}`,
+    output: `const foo = function (/*comment2*/) {};
+`,
+  },
+  {
+    name: 'empty function expression with comment #3',
+    input: `const foo = function () /*comment3*/ {}`,
+    output: `const foo = function () /*comment3*/ {};
+`,
+  },
+  {
+    name: 'empty function expression with comment #4',
+    input: `const foo = function ()
+/*comment4*/
+{}`,
+    output: `const foo = function () /*comment4*/
+{};
+`,
+  },
+  {
+    name: 'empty function expression with comment #5',
+    input: `const foo = function () {} /*comment5*/`,
+    output: `const foo = function () {}; /*comment5*/
+`,
+  },
+  {
+    name: 'empty arrow function expression',
+    input: `const foo = () => {}`,
+    output: `const foo = () => {};
+`,
+  },
+  {
+    name: 'empty arrow function expression with comment #1',
+    input: `const foo = (/*comment1*/) => {}`,
+    output: `const foo = (/*comment1*/) => {};
+`,
+  },
+  {
+    name: 'empty arrow function expression with comment #2',
+    input: `const foo = () /*comment2*/ => {}`,
+    output: `const foo = () /*comment2*/ => {};
+`,
+  },
+  {
+    name: 'empty arrow function expression with comment #3',
+    input: `const foo = () => /*comment3*/ {}`,
+    output: `const foo = () => /*comment3*/ {};
+`,
+  },
+  {
+    name: 'empty arrow function expression with comment #4',
+    input: `const foo = () =>
+/*comment4*/
+{}`,
+    output: `const foo = () =>
+  /*comment4*/
+  {};
+`,
+  },
+  {
+    name: 'empty arrow function expression with comment #5',
+    input: `const foo = () => {} /*comment5*/`,
+    output: `const foo = () => {}; /*comment5*/
+`,
+  },
+  {
+    name: 'anonymous function expression',
+    input: `(function () {})`,
+    output: `(function () {});
+`,
+  },
+  {
+    name: 'IIFE #1',
+    input: `(function () {})()`,
+    output: `(function () {})();
+`,
+  },
+  {
+    name: 'IIFE #2',
+    input: `(() => {})()`,
+    output: `(() => {})();
+`,
+  },
+  {
+    name: 'promise chain',
+    input: `myPromise.then((value) => { console.log(value) }).catch((err) => { console.error(err) })`,
+    output: `myPromise
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+`,
+  },
+  {
+    name: 'object literal parameter #1',
+    input: `foo({
+  bar,
+  baz: 'baz'
+})`,
+    output: `foo({
+  bar,
+  baz: "baz",
+});
+`,
+  },
+  {
+    name: 'object literal parameter #2',
+    input: `foo(bar, {
+  baz: 'baz'
+})`,
+    output: `foo(bar, {
+  baz: "baz",
+});
+`,
+  },
+  {
+    name: 'object literal parameter #3',
+    input: `foo({bar: 'bar'}, {
+  baz: 'baz'
+})`,
+    output: `foo(
+  { bar: "bar" },
+  {
+    baz: "baz",
+  },
+);
+`,
+  },
+  {
+    name: 'function that returns object literal #1',
+    input: `function foo() { return {} }`,
+    output: `function foo() {
+  return {};
+}
+`,
+  },
+  {
+    name: 'function that returns object literal #2',
+    input: `function foo() { return { foo: 'bar' } }`,
+    output: `function foo() {
+  return { foo: "bar" };
+}
+`,
+  },
+  {
+    name: 'function that returns object literal #3',
+    input: `function foo() {
+  return {
+    foo: 'bar',
+    baz,
+  }
+}`,
+    output: `function foo() {
+  return {
+    foo: "bar",
+    baz,
+  };
+}
+`,
+  },
+  {
+    name: 'arrow function that returns object literal #1',
+    input: `const foo = () => ({})`,
+    output: `const foo = () => ({});
+`,
+  },
+  {
+    name: 'arrow function that returns object literal #2',
+    input: `const foo = () => ({ foo: 'bar' })`,
+    output: `const foo = () => ({ foo: "bar" });
+`,
+  },
+  {
+    name: 'arrow function that returns object literal #3',
+    input: `const foo = () => ({
+  foo: 'bar',
+  baz,
+})`,
+    output: `const foo = () => ({
+  foo: "bar",
+  baz,
+});
+`,
+  },
+];
+
+describe('babel/function/stroustrup', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
---
+++ tests/babel/prettier-ignore/stroustrup.test.ts
@@ -0,0 +1,67 @@
+import type { Fixture } from '../../settings';
+import { format, baseOptions } from '../../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'babel',
+  braceStyle: 'stroustrup',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'ignore comment #1',
+    input: `// prettier-ignore
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}`,
+    output: `// prettier-ignore
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+`,
+  },
+  {
+    name: 'ignore comment #2',
+    input: `/* prettier-ignore */
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}`,
+    output: `/* prettier-ignore */
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+`,
+  },
+];
+
+describe('babel/prettier-ignore/stroustrup', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
