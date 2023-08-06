d380968acb1171b4d94702d63c47fd00a3a9e7b9
Hyeonjong <nianelo4@gmail.com>
2023-08-06 14:47:57 +0900

    test: add various tests for '1tbs' option

--- tests/settings.ts
+++ tests/settings.ts
@@ -2,6 +2,12 @@ import type { Options } from 'prettier';
 
 import braceStylePlugin from '@/index';
 
+export type Fixture = {
+  name: string;
+  input: string;
+  output: string;
+};
+
 export { format } from 'prettier';
 
 export const baseOptions: Options = {
