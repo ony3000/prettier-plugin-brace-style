a8ca809e8fcf5dfcd5095fc3f6179eeeb62ff979
Hyeonjong <nianelo4@gmail.com>
2023-08-11 22:54:56 +0900

    test: add 'namespace' syntax tests

--- tests/typescript/namespace/allman.test.ts
+++ tests/typescript/namespace/allman.test.ts
@@ -7,7 +7,78 @@ const options = {
   braceStyle: 'allman',
 };
 
-const fixtures: Fixture[] = [];
+const fixtures: Fixture[] = [
+  {
+    name: 'namespacing',
+    input: `
+namespace Validation {
+  export interface StringValidator {
+    isAcceptable(s: string): boolean;
+  }
+}
+`,
+    output: `namespace Validation
+{
+  export interface StringValidator
+  {
+    isAcceptable(s: string): boolean;
+  }
+}
+`,
+  },
+  {
+    name: 'alias',
+    input: `
+namespace Shapes {
+  export namespace Polygons {
+    export class Triangle {}
+    export class Square {}
+  }
+}
+
+import polygons = Shapes.Polygons;
+let sq = new polygons.Square(); // Same as 'new Shapes.Polygons.Square()'
+`,
+    output: `namespace Shapes
+{
+  export namespace Polygons
+  {
+    export class Triangle
+    {}
+    export class Square
+    {}
+  }
+}
+
+import polygons = Shapes.Polygons;
+let sq = new polygons.Square(); // Same as 'new Shapes.Polygons.Square()'
+`,
+  },
+  {
+    name: 'ambient namespace',
+    input: `
+declare namespace D3 {
+  export interface Selectors {
+    select: {
+      (selector: string): Selection;
+      (element: EventTarget): Selection;
+    };
+  }
+}
+`,
+    output: `declare namespace D3
+{
+  export interface Selectors
+  {
+    select: {
+      (selector: string): Selection;
+      (element: EventTarget): Selection;
+    };
+  }
+}
+`,
+  },
+];
 
 describe('typescript/namespace/allman', () => {
   for (const fixture of fixtures) {
--- tests/typescript/namespace/stroustrup.test.ts
+++ tests/typescript/namespace/stroustrup.test.ts
@@ -7,7 +7,70 @@ const options = {
   braceStyle: 'stroustrup',
 };
 
-const fixtures: Fixture[] = [];
+const fixtures: Fixture[] = [
+  {
+    name: 'namespacing',
+    input: `
+namespace Validation {
+  export interface StringValidator {
+    isAcceptable(s: string): boolean;
+  }
+}
+`,
+    output: `namespace Validation {
+  export interface StringValidator {
+    isAcceptable(s: string): boolean;
+  }
+}
+`,
+  },
+  {
+    name: 'alias',
+    input: `
+namespace Shapes {
+  export namespace Polygons {
+    export class Triangle {}
+    export class Square {}
+  }
+}
+
+import polygons = Shapes.Polygons;
+let sq = new polygons.Square(); // Same as 'new Shapes.Polygons.Square()'
+`,
+    output: `namespace Shapes {
+  export namespace Polygons {
+    export class Triangle {}
+    export class Square {}
+  }
+}
+
+import polygons = Shapes.Polygons;
+let sq = new polygons.Square(); // Same as 'new Shapes.Polygons.Square()'
+`,
+  },
+  {
+    name: 'ambient namespace',
+    input: `
+declare namespace D3 {
+  export interface Selectors {
+    select: {
+      (selector: string): Selection;
+      (element: EventTarget): Selection;
+    };
+  }
+}
+`,
+    output: `declare namespace D3 {
+  export interface Selectors {
+    select: {
+      (selector: string): Selection;
+      (element: EventTarget): Selection;
+    };
+  }
+}
+`,
+  },
+];
 
 describe('typescript/namespace/stroustrup', () => {
   for (const fixture of fixtures) {
