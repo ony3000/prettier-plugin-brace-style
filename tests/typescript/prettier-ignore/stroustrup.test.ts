6c18efe981d0fc90973d416a67c3e9613dd289ec
Hyeonjong <nianelo4@gmail.com>
2023-08-26 11:01:34 +0900

    test: update 'prettier-ignore' syntax tests

--- tests/typescript/prettier-ignore/stroustrup.test.ts
+++ tests/typescript/prettier-ignore/stroustrup.test.ts
@@ -56,6 +56,188 @@ else
 }
 `,
   },
+  {
+    name: 'ignore comment #3',
+    input: `
+// prettier-ignore
+export function MyComponent() {
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
+`,
+    output: `// prettier-ignore
+export function MyComponent() {
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'ignore comment #4',
+    input: `
+export function MyComponent() {
+  return (
+    <div>
+      {/* prettier-ignore */}
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
+`,
+    output: `export function MyComponent() {
+  return (
+    <div>
+      {/* prettier-ignore */}
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+      <button type="button" onClick={() => {}}>
+        Click me
+      </button>
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'ignore comment #5',
+    input: `
+export function MyComponent() {
+  return (
+    <div>
+      <button
+        type="button"
+        // prettier-ignore
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+      <button
+        type="button"
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
+`,
+    output: `export function MyComponent() {
+  return (
+    <div>
+      <button
+        type="button"
+        // prettier-ignore
+        onClick={() => {}}
+      >
+        Click me
+      </button>
+      <button type="button" onClick={() => {}}>
+        Click me
+      </button>
+    </div>
+  );
+}
+`,
+  },
+  {
+    name: 'comments that contain the phrase `prettier-ignore` but do not prevent formatting #1',
+    input: `
+/**
+ * prettier-ignore
+ */
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+`,
+    output: `/**
+ * prettier-ignore
+ */
+if (condition1) {
+  foo;
+}
+else if (condition2) {
+  bar;
+}
+else {
+  baz;
+}
+`,
+  },
+  {
+    name: 'comments that contain the phrase `prettier-ignore` but do not prevent formatting #2',
+    input: `
+// /* prettier-ignore */
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+`,
+    output: `// /* prettier-ignore */
+if (condition1) {
+  foo;
+}
+else if (condition2) {
+  bar;
+}
+else {
+  baz;
+}
+`,
+  },
 ];
 
 describe('typescript/prettier-ignore/stroustrup', () => {
