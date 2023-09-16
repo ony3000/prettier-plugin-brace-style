ab215275b50afe698014f7a3cc0a9c0b15b7e1b6
Hyeonjong <nianelo4@gmail.com>
2023-09-16 15:15:18 +0900

    build: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,28 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'PrettierPluginBraceStyle',
-      fileName: 'prettier-plugin-brace-style',
+      entry: [
+        resolve(__dirname, 'src/v2-plugin.ts'),
+        resolve(__dirname, 'src/v3-plugin.ts'),
+        resolve(__dirname, 'src/adaptors/worker.js'),
+      ],
+      formats: ['es', 'cjs'],
+      fileName: (format, entryName) => {
+        const extension = format === 'cjs' ? '.cjs' : '.js';
+
+        switch (entryName) {
+          case 'worker':
+            return `worker${extension}`;
+          case 'v3-plugin':
+            return `prettier3-plugin-brace-style${extension}`;
+          case 'v2-plugin':
+          default:
+            return `prettier-plugin-brace-style${extension}`;
+        }
+      },
     },
     rollupOptions: {
-      external: [/^prettier/],
+      external: [/^prettier/, 'path', 'worker_threads'],
       output: {
         globals: {
           prettier: 'prettier',
