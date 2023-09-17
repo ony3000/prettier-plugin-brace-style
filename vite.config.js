b080a7759c886f29be5846bf2a535432a7e81211
Hyeonjong <nianelo4@gmail.com>
2023-09-17 21:33:11 +0900

    chore: remove unnecessary things

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,12 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
-      entry: [
-        resolve(__dirname, 'src/v2-plugin.ts'),
-        resolve(__dirname, 'src/v3-plugin.ts'),
-        resolve(__dirname, 'src/adaptors/worker.js'),
-      ],
+      entry: [resolve(__dirname, 'src/v2-plugin.ts'), resolve(__dirname, 'src/v3-plugin.ts')],
       formats: ['es', 'cjs'],
       fileName: (format, entryName) => {
         const extension = format === 'cjs' ? '.cjs' : '.js';
 
         switch (entryName) {
-          case 'worker':
-            return `worker${extension}`;
           case 'v3-plugin':
             return `prettier3-plugin-brace-style${extension}`;
           case 'v2-plugin':
