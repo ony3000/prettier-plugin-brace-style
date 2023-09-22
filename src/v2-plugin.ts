bda382fe4eaa6f91ce2c9140680762c47099bb2e
Hyeonjong <nianelo4@gmail.com>
2023-09-22 23:00:53 +0900

    feat: now uses named export instead of default export

--- src/v2-plugin.ts
+++ src/v2-plugin.ts
@@ -1,13 +1,3 @@
-import type { Plugin } from 'prettier';
-
-import { options } from './v2-parts/options';
-import { parsers } from './v2-parts/parsers';
-import { printers } from './v2-parts/printers';
-
-const braceStylePlugin: Plugin = {
-  parsers,
-  printers,
-  options,
-};
-
-export default braceStylePlugin;
+export { options } from './v2-parts/options';
+export { parsers } from './v2-parts/parsers';
+export { printers } from './v2-parts/printers';
--- src/v3-plugin.ts
+++ src/v3-plugin.ts
@@ -1,13 +1,3 @@
-import type { Plugin } from 'prettier3';
-
-import { options } from './v3-parts/options';
-import { parsers } from './v3-parts/parsers';
-import { printers } from './v3-parts/printers';
-
-const braceStylePlugin: Plugin = {
-  parsers,
-  printers,
-  options,
-};
-
-export default braceStylePlugin;
+export { options } from './v3-parts/options';
+export { parsers } from './v3-parts/parsers';
+export { printers } from './v3-parts/printers';
