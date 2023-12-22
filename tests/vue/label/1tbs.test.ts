b82da84904770a14ee4b3320093bd14867afe8a3
Hyeonjong <nianelo4@gmail.com>
2023-12-22 22:49:21 +0900

    test: add vue tests for '1tbs' option

---
+++ tests/vue/label/1tbs.test.ts
@@ -0,0 +1,75 @@
+import type { Fixture } from '../../settings';
+import { format, baseOptions } from '../../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'vue',
+  braceStyle: '1tbs',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'labeled block',
+input: `
+<script setup lang="ts">
+foo: {
+  console.log('face');
+  break foo;
+  console.log('this will not be executed');
+}
+console.log('swap');
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="() => {
+foo: {
+  console.log('face');
+  break foo;
+  console.log('this will not be executed');
+}
+console.log('swap');
+    }"
+  >
+    Click Me
+  </button>
+</template>
+`,
+output: `<script setup lang="ts">
+foo: {
+  console.log("face");
+  break foo;
+  console.log("this will not be executed");
+}
+console.log("swap");
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="
+      () => {
+        foo: {
+          console.log('face');
+          break foo;
+          console.log('this will not be executed');
+        }
+        console.log('swap');
+      }
+    "
+  >
+    Click Me
+  </button>
+</template>
+`,
+  },
+];
+
+describe('vue/label/1tbs', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
