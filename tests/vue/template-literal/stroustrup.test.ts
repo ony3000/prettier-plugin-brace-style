002c6c9a9b63619d33a0a503bcf5a17085e1db52
Hyeonjong <nianelo4@gmail.com>
2023-12-23 13:45:28 +0900

    test: add vue tests for 'stroustrup' option

---
+++ tests/vue/template-literal/stroustrup.test.ts
@@ -0,0 +1,143 @@
+import type { Fixture } from '../../settings';
+import { format, baseOptions } from '../../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'vue',
+  braceStyle: 'stroustrup',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'template literal',
+input: `
+<script setup lang="ts">
+const x = \`
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+\`
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="() => {
+const x = \`
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+\`
+    }"
+  >
+    Click Me
+  </button>
+</template>
+`,
+output: `<script setup lang="ts">
+const x = \`
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+\`;
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="
+      () => {
+        const x = \`
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+\`;
+      }
+    "
+  >
+    Click Me
+  </button>
+</template>
+`,
+  },
+  {
+    name: 'nested template literal',
+input: `
+<script setup lang="ts">
+const x = \`foo: \${1 + (function () { return 2; })() + 3}\`
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="() => {
+const x = \`foo: \${1 + (function () { return 2; })() + 3}\`
+    }"
+  >
+    Click Me
+  </button>
+</template>
+`,
+output: `<script setup lang="ts">
+const x = \`foo: \${
+  1 +
+  (function () {
+    return 2;
+  })() +
+  3
+}\`;
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="
+      () => {
+        const x = \`foo: \${
+          1 +
+          (function () {
+            return 2;
+          })() +
+          3
+        }\`;
+      }
+    "
+  >
+    Click Me
+  </button>
+</template>
+`,
+  },
+];
+
+describe('vue/template-literal/stroustrup', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, () => {
+      expect(format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
