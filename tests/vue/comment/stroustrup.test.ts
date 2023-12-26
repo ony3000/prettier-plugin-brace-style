90d071ba2e262d3ec0dc6dd1a8ef05ba08e6d874
Hyeonjong <nianelo4@gmail.com>
2023-12-26 22:08:10 +0900

    test: add vue tests for 'stroustrup' option

---
+++ tests/vue/comment/stroustrup.test.ts
@@ -0,0 +1,159 @@
+import type { Fixture } from '../../settings';
+import { format, baseOptions } from '../../settings';
+
+const options = {
+  ...baseOptions,
+  parser: 'vue',
+  braceStyle: 'stroustrup',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'single line comment',
+    input: `
+<script setup lang="ts">
+//class Foo {}
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="() => {
+//class Foo {}
+    }"
+  >
+    Click Me
+  </button>
+</template>
+`,
+    output: `<script setup lang="ts">
+//class Foo {}
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="
+      () => {
+        //class Foo {}
+      }
+    "
+  >
+    Click Me
+  </button>
+</template>
+`,
+  },
+  {
+    name: 'single line comment applied to multi line',
+    input: `
+<script setup lang="ts">
+// function foo() {
+//   bar;
+// }
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="() => {
+// function foo() {
+//   bar;
+// }
+    }"
+  >
+    Click Me
+  </button>
+</template>
+`,
+    output: `<script setup lang="ts">
+// function foo() {
+//   bar;
+// }
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="
+      () => {
+        // function foo() {
+        //   bar;
+        // }
+      }
+    "
+  >
+    Click Me
+  </button>
+</template>
+`,
+  },
+  {
+    name: 'multi line comment',
+    input: `
+<script setup lang="ts">
+/*
+if (foo) {
+  bar();
+} else {
+  baz();
+}
+*/
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="() => {
+/*
+if (foo) {
+  bar();
+} else {
+  baz();
+}
+*/
+    }"
+  >
+    Click Me
+  </button>
+</template>
+`,
+    output: `<script setup lang="ts">
+/*
+if (foo) {
+  bar();
+} else {
+  baz();
+}
+*/
+</script>
+
+<template>
+  <button
+    type="button"
+    @click="
+      () => {
+        /*
+if (foo) {
+  bar();
+} else {
+  baz();
+}
+*/
+      }
+    "
+  >
+    Click Me
+  </button>
+</template>
+`,
+  },
+];
+
+describe('vue/comment/stroustrup', () => {
+  for (const fixture of fixtures) {
+    test(fixture.name, async () => {
+      expect(await format(fixture.input, options)).toBe(fixture.output);
+    });
+  }
+});
