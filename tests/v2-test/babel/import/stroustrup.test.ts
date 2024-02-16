88d6c17db5189ee1c4aeccc7ca961234e313fda3
Hyeonjong <nianelo4@gmail.com>
2024-02-16 20:55:53 +0900

    test: add revamped babel tests

---
+++ tests/v2-test/babel/import/stroustrup.test.ts
@@ -0,0 +1,83 @@
+import { format } from 'prettier';
+import type { Fixture } from 'test-settings';
+import { baseOptions } from 'test-settings';
+import { stroustrupLinter } from 'test-settings/linter';
+import { describe, expect, test } from 'vitest';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as thisPlugin from '@/packages/v2-plugin';
+
+const options = {
+  ...baseOptions,
+  plugins: [thisPlugin],
+  parser: 'babel',
+  braceStyle: 'stroustrup',
+};
+
+const fixtures: Fixture[] = [
+  {
+    name: 'named import (1)',
+    input: `import { foo } from 'foo'`,
+    output: `import { foo } from "foo";
+`,
+  },
+  {
+    name: 'named import (2)',
+    input: `import {
+  foo,
+  bar,
+  baz,
+} from 'foo'`,
+    output: `import { foo, bar, baz } from "foo";
+`,
+  },
+  {
+    name: 'named import (3)',
+    input: `import {
+  foo,
+  bar,
+  baz,
+  foobar,
+  fooBaz,
+  barFoo,
+  barBaz,
+  bazFoo,
+  bazBar,
+  fooBarBaz,
+} from 'foo'`,
+    output: `import {
+  foo,
+  bar,
+  baz,
+  foobar,
+  fooBaz,
+  barFoo,
+  barBaz,
+  bazFoo,
+  bazBar,
+  fooBarBaz,
+} from "foo";
+`,
+  },
+];
+
+describe('babel/import/stroustrup', () => {
+  for (const fixture of fixtures) {
+    const formattedText = format(fixture.input, {
+      ...options,
+      ...(fixture.options ?? {}),
+    });
+
+    describe(fixture.name, () => {
+      test('theoretical', async () => {
+        const [result] = await stroustrupLinter.lintText(formattedText);
+
+        expect(result.fixableErrorCount).toBe(0);
+      });
+
+      test('practical', () => {
+        expect(formattedText).toBe(fixture.output);
+      });
+    });
+  }
+});
