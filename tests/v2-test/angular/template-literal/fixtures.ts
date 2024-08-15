d7228cbcbf677b03e9e9333e4055cd9a88b13b1f
Hyeonjong <nianelo4@gmail.com>
2024-08-16 00:06:41 +0900

    test: add angular parser tests

---
+++ tests/v2-test/angular/template-literal/fixtures.ts
@@ -0,0 +1,30 @@
+import type { Fixture } from 'test-settings';
+
+export const fixtures: Omit<Fixture, 'output'>[] = [
+  {
+    name: 'template literal',
+    input: `
+<script lang="ts">
+const x = \`
+if (condition1) {
+  foo
+} else if (condition2) {
+  bar
+}
+else
+{
+  baz
+}
+\`
+</script>
+`,
+  },
+  {
+    name: 'nested template literal',
+    input: `
+<script lang="ts">
+const x = \`foo: \${1 + (function () { return 2; })() + 3}\`
+</script>
+`,
+  },
+];
