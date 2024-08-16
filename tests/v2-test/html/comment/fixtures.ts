f05e79a0712003a4b01ed1894f2da3aedde84596
Hyeonjong <nianelo4@gmail.com>
2024-08-16 20:24:43 +0900

    test: add html parser tests

---
+++ tests/v2-test/html/comment/fixtures.ts
@@ -0,0 +1,36 @@
+import type { Fixture } from 'test-settings';
+
+export const fixtures: Omit<Fixture, 'output'>[] = [
+  {
+    name: 'single line comment (1)',
+    input: `
+<script lang="ts">
+//class Foo {}
+</script>
+`,
+  },
+  {
+    name: 'single line comment (2) - applied to multi line',
+    input: `
+<script lang="ts">
+// function foo() {
+//   bar;
+// }
+</script>
+`,
+  },
+  {
+    name: 'multi line comment',
+    input: `
+<script lang="ts">
+/*
+if (foo) {
+  bar();
+} else {
+  baz();
+}
+*/
+</script>
+`,
+  },
+];
