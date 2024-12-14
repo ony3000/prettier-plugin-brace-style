658d9cce295a4160c6ffa753c81c1e22d01b321c
Hyeonjong <nianelo4@gmail.com>
2024-12-14 11:35:18 +0900

    test: add code block tests

---
+++ tests/v3-test/markdown/code-block/stroustrup.test.ts
@@ -0,0 +1,26 @@
+import { format } from 'prettier';
+import { baseOptions } from 'test-settings';
+import { expect, test } from 'vitest';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as thisPlugin from '@/packages/v3-plugin';
+
+import { fixtures } from './fixtures';
+
+const options = {
+  ...baseOptions,
+  plugins: [thisPlugin],
+  parser: 'markdown',
+  braceStyle: 'stroustrup',
+};
+
+for (const fixture of fixtures) {
+  test(fixture.name, async () => {
+    expect(
+      await format(fixture.input, {
+        ...options,
+        ...(fixture.options ?? {}),
+      }),
+    ).toMatchSnapshot();
+  });
+}
---
+++ tests/v3-test/mdx/code-block/fixtures.ts
@@ -0,0 +1,62 @@
+import type { Fixture } from 'test-settings';
+
+export const fixtures: Omit<Fixture, 'output'>[] = [
+  {
+    name: "(1) This plugin doesn't support markdown parser, so it leaves Prettier's output as is.",
+    input: `
+\`\`\`jsx
+<div />;
+\`\`\`
+`,
+    options: {},
+  },
+  {
+    name: "(2) This plugin doesn't support markdown parser, so it leaves Prettier's output as is.",
+    input: `
+\`\`\`jsx
+import foo from 'foo'
+
+<div />
+\`\`\`
+`,
+    options: {},
+  },
+  {
+    name: "(3) This plugin doesn't support markdown parser, so it leaves Prettier's output as is.",
+    input: `
+\`\`\`jsx
+if (foo) { bar(); } else { baz(); }
+\`\`\`
+`,
+    options: {},
+  },
+  {
+    name: "(4) This plugin doesn't support markdown parser, so it leaves Prettier's output as is.",
+    input: `
+\`\`\`tsx
+<div />;
+\`\`\`
+`,
+    options: {},
+  },
+  {
+    name: "(5) This plugin doesn't support markdown parser, so it leaves Prettier's output as is.",
+    input: `
+\`\`\`tsx
+import foo from 'foo'
+
+<div />
+\`\`\`
+`,
+    options: {},
+  },
+  {
+    name: "(6) This plugin doesn't support markdown parser, so it leaves Prettier's output as is.",
+    input: `
+\`\`\`tsx
+if (foo) { bar(); } else { baz(); }
+\`\`\`
+`,
+    options: {},
+  },
+];
