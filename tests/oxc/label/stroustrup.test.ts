d69f6ba70b813cbd05d9667199dc7931407a8f76
Hyeonjong <nianelo4@gmail.com>
2026-01-06 00:29:17 +0900

    test: copy babel tests to oxc tests

---
+++ tests/oxc/label/stroustrup.test.ts
@@ -0,0 +1,25 @@
+import { format } from 'prettier';
+import { expect, test } from 'vitest';
+
+import * as thisPlugin from '@/index';
+
+import { baseOptions } from '../../settings';
+import { fixtures } from './fixtures';
+
+const options = {
+  ...baseOptions,
+  plugins: ['@prettier/plugin-oxc', thisPlugin],
+  parser: 'oxc',
+  braceStyle: 'stroustrup',
+};
+
+for (const fixture of fixtures) {
+  test(fixture.name, async () => {
+    expect(
+      await format(fixture.input, {
+        ...options,
+        ...(fixture.options ?? {}),
+      }),
+    ).toMatchSnapshot();
+  });
+}
